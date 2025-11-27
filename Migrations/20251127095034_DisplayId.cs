850e3ae0248eefcf8ea25d1fd122175921688076
Patricija Videc <160044511+PatricijaVidec@users.noreply.github.com>
2025-11-27 10:56:51 +0100

    migrated displayid

---
+++ Migrations/20251127095034_DisplayId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartPark.Migrations
+{
+    /// <inheritdoc />
+    public partial class DisplayId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DisplayId",
+                table: "ParkingSpot",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayId",
+                table: "ParkingSpot");
+        }
+    }
+}
